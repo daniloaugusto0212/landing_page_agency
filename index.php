db1aa8c70413a775c9c9621f7c31befd32c736c5
Danilo <daniloaugusto0212@hotmail.com>
2021-02-16 22:08:46 -0300

    Header a background da primeira seção

--- index.php
+++ index.php
@@ -7,9 +7,37 @@
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto_03</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="images/new-logo.png" alt="Logomarca sitedan criação de sites e sistemas web">
+            </div>
+            <!-- /.logo -->
+            <nav>
+                <ul>
+                    <li><a href="">home</a></li>
+                    <li><a href="">serviços</a></li>
+                    <li><a href="">sobre</a></li>
+                    <li><a href="">contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+            <!-- /.clear -->
+
+            <div class="chamada1">
+                <h2>Sistemas Próprios<br> Para Empresas Autênticas</h2>
+                <p>CRIAÇÃO DE SITES E SISTEMAS WEB | SITES ATRAENTES E AMIGÁVEIS ​​AO USUÁRIO.</p>
+            </div>
+            <!-- /.chamada1 -->
+        </div>
+        <!-- /.container -->
+    </header>
     
 </body>
 </html>
