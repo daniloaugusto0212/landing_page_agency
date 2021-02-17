6acd62cff9bc2ce7098184ba5cdcd843d1b4fa0b
Danilo <daniloaugusto0212@hotmail.com>
2021-02-16 21:38:33 -0300

    Estruturando arquivos

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Criação de sites e sistemas Web">
+    <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto_03</title>
     <link rel="stylesheet" href="css/style.css">
