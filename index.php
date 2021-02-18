edc4e10711ec4a2b34bea320d4c970ca9599c2db
Danilo <daniloaugusto0212@hotmail.com>
2021-02-17 21:51:37 -0300

    Criando menu responsivo

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>
 
@@ -29,6 +30,7 @@
             </nav>
 
             <nav class="mobile-menu">
+                <i class="fas fa-bars"></i>
                 <ul>
                     <li><a href="">home</a></li>
                     <li><a href="">serviços</a></li>
